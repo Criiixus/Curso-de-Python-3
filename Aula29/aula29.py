3a974b7edc45fe4dbc8e3ad58756b7179da14884
CristianLeme <cristiandonascimento03@gmail.com>
2025-08-28 23:32:33 -0300

    Renomeando pastas com zeros à esquerda

--- Aula29/aula29.py
+++ Aula29/aula29.py
@@ -3,3 +3,11 @@ Introducao ao try/except
 try -> tentar executar o codigo
 except -> ocorreu algum erro ao tentar executar"""
 
+numero_str = input('Vou dobrar o numero que vc digitar: ')
+
+try:
+    numero_float = float(numero_str)
+    print('Float',numero_float)
+    print(f'O dobro de {numero_str} e {numero_float * 2:.2}')
+except:
+    print('Isso nao e um numero')
---
+++ Aula31/aula31.py
@@ -0,0 +1,16 @@
+"""
+Flag (Bandeira) - Marcar um local 
+Nome = nao valor 
+is e is not = e ou nao e (tipo, valor, identidade)
+id = identidade 
+"""
+
+
+
+condicao = False 
+
+
+if condicao:
+    print('Faca algo')
+else:
+    print('Nao faca algo')
