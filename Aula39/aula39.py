6ff7655063d293255cf86f7f79e1b81e0694b1a2
CristianLeme <cristiandonascimento03@gmail.com>
2025-09-10 22:47:10 -0300

    10-09

--- Aula39/aula39.py
+++ Aula39/aula39.py
@@ -3,3 +3,13 @@
 nome = 'Cristian'
 #       87654321
 
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
+
---
+++ Aula41/aula41.py
@@ -0,0 +1,11 @@
+""" while/else """
+
+string= 'Valor qualquer'
+
+i = 0
+while i < len(string):
+    letra = string[1]
+
+    print(letra)
+    i += 1
+    
